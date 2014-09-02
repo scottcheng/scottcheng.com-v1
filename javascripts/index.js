130b6385d2ff1aab624df45ecad25a7fe23d9810
Scott Cheng <me@scottcheng.com>
2014-09-01 23:27:24 -0700

    Deploy

---
+++ javascripts/index.js
@@ -0,0 +1,40 @@
+$(function() {
+  var $crclWrappers = $('.crcl-wrapper')
+    , $crcls = $('.crcl')
+    , $window = $(window)
+    , $name = $('#name')
+    , $links = $('#links');
+
+  var resize = function() {
+    var windowWidth = $window.width();
+
+    // Fix the circle sizes
+    var crclWrapperWidth = windowWidth * 0.3;
+    $crclWrappers.width(crclWrapperWidth);
+    if (!$crclWrappers.hasClass('expand')) {
+      $crclWrappers.height(crclWrapperWidth);
+      $crcls
+        .width(crclWrapperWidth * 2)
+        .height(crclWrapperWidth * 2);
+    }
+
+    // Change font size of #name
+    if (windowWidth <= 1280 && windowWidth >= 1122) {
+      $name.css('fontSize', '3.6em')
+    } else {
+      $name.css('fontSize', '4.2em')
+    }
+  };
+  
+  resize();
+  $window.resize(function() {
+    resize();
+  });
+
+  // Circle expansion effect
+  $links.hover(function() {
+    $crclWrappers.width($window.width() * 0.6);
+  }, function() {
+    $crclWrappers.width($window.width() * 0.3);
+  });
+});
